39affb7ffb704f5fdeee4f964437b5d32344e879
ctroupin <ctroupin@uliege.be>
2024-06-06 22:05:08 +0200

    reading bird data

--- src/R/read_bird_data.R
+++ src/R/read_bird_data.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 datadir <- "/home/ctroupin/data/EMODnet/Biology/dwca-esas-v1.3"
 
 eventfile = file.path(datadir, "event200.csv")
@@ -6,6 +8,30 @@ occurfile = file.path(datadir, "occurrence200.csv")
 file.exists(eventfile)
 file.exists(occurfile)
 
-
+# Read the event data
+# we only need the time, coordinates and the event IDs 
 eventdata <- read.csv(eventfile, sep="\t", header=TRUE)
-evenID <- eventdata$eventID
+eventID <- eventdata$eventID
+lon <- eventdata$decimalLongitude
+lat <- eventdata$decimalLatitude
+dates <- eventdata$eventDate
+
+# Basic plot showing the positions
+ggplot(eventdata, aes(x=lon, y=lat) ) +
+  geom_point()
+
+# Read the occurrence data
+occurdata <- read.csv(occurfile, sep="\t", header=TRUE)
+scientificname <- occurdata$scientificName
+individualcount <- occurdata$individualCount
+eventIDoccur <- occurdata$eventID
+
+scientificnameUnique = unique(scientificname)
+
+# Find the event corresponding to the species selected
+myspecies = "Larus fuscus"
+speciesindex = (scientificname == myspecies)
+nselec = sum(speciesindex)
+speciesevent = eventIDoccur[speciesindex]
+
+
